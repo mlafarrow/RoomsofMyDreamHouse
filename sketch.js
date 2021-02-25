f49c5d3c755ae0ac2cb106d2ce621e73678e6579
emily farrow <ejfarrow@dons.usfca.edu>
2021-02-24 22:47:44 -0800

    Added Initial Files

---
+++ sketch.js
@@ -0,0 +1,23 @@
+/*************************************************************************
+    (*)Project Name
+          (*)by author's name/s
+
+    (*)Overview - what the project does...
+ 
+    ---------------------------------------------------------------------
+    (*)Notes: (ie: To be fixed, I Need Help With, Clarify This, etc etc: )
+     (1)
+     (2)
+**************************************************************************/
+
+
+// Setup code goes here
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+}
